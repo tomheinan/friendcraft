b5eabf895673e5f0b365754812e5c24fcaad8c91
Tom Heinan <theinan@onemedical.com>
2014-04-27 14:04:39 -0700

    Added new FriendCraft plugin auth

--- src/main/java/com/tomheinan/friendcraft/FriendCraft.java
+++ src/main/java/com/tomheinan/friendcraft/FriendCraft.java
@@ -1,29 +1,43 @@
 package com.tomheinan.friendcraft;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.bukkit.Server;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.firebase.client.DataSnapshot;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firebase.client.Firebase;
+import com.firebase.client.Firebase.AuthListener;
+import com.firebase.client.Firebase.CompletionListener;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
 public class FriendCraft extends JavaPlugin
 {
-    public static boolean debugMode = false;
+    //public static String firebaseRoot = "https://friendcraft.firebaseio.com"; // production
+    public static String firebaseRoot = "https://friendcraft-dev.firebaseio.com"; // development
+    
+    //public static String authRoot = "https://friendcraft.io"; // production
+    public static String authRoot = "http://localhost:3000"; // development
+    
     protected static Logger logger = null;
     
     protected Server server;
     protected File dataFolder;
     protected EventListener eventListener;
+    protected Object authData;
 
     public void onEnable()
     {
@@ -35,27 +49,86 @@ public class FriendCraft extends JavaPlugin
         // plugin configuration shenanigans
         Configuration configuration = this.getConfig();
         configuration.options().copyDefaults(true);
-        debugMode = configuration.getBoolean("debug");
-        int port = configuration.getInt("port", 25566);
         this.saveConfig();
         
-        // register for bukkit events
-        if (this.eventListener == null) { this.eventListener = new EventListener(); }
-        this.server.getPluginManager().registerEvents(this.eventListener, this);
+        // authenticate with firebase
+        Map<String, Object> credentials = configuration.getConfigurationSection("authentication").getValues(false);
+        String id = (String) credentials.get("id");
+        String secret = (String) credentials.get("secret");
         
-        // firebase stuff
-        Firebase ref = new Firebase("https://friendcraft.firebaseio.com/");
-        ref.setValue("testing 123");
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> jsonPayload = new HashMap<String, String>();
+        jsonPayload.put("type", "plugin");
+        jsonPayload.put("id", id);
+        jsonPayload.put("secret", secret);
         
-        // Read data and react to changes
-        ref.addValueEventListener(new ValueEventListener() {
-            
-            public void onDataChange(DataSnapshot snap) {
-                logger.info(snap.getName() + " -> " + snap.getValue());
+        StringEntity body = null;
+        try {
+            body = new StringEntity(mapper.writeValueAsString(jsonPayload));
+        } catch (Exception e) {
+            error("Unable to serialize FriendCraft plugin credentials", e);
+            disable();
+            return;
+        }
+        
+        HttpPost postRequest = new HttpPost(authRoot + "/auth");
+        postRequest.addHeader("Content-Type", "application/json");
+        postRequest.addHeader("Accept", "application/json");
+        postRequest.setEntity(body);
+        
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = null;
+        try {
+            log("Authenticating as \"" + id + "\"");
+            response = httpClient.execute(postRequest);
+        } catch (Exception e) {
+            error("Unable to reach FriendCraft authentication server", e);
+            disable();
+            return;
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+        
+        if (response.getStatusLine().getStatusCode() != 200) {
+            error("Your plugin credentials are incorrect; please check them in config.yml");
+            disable();
+            return;
+        }
+        
+        Map<String, String> jsonResponse = null;
+        try {
+            jsonResponse = mapper.readValue(EntityUtils.toString(response.getEntity()), new TypeReference<HashMap<String, String>>(){});
+        } catch (Exception e) {
+            error("Unable to parse FriendCraft authorization API response", e);
+            disable();
+            return;
+        }
+        
+        String token = jsonResponse.get("token");
+        //TODO get the expiry here too so we can set a timer to renew
+        
+        log("Connecting to Firebase");
+        Firebase rootRef = new Firebase(firebaseRoot);
+        rootRef.auth(token, new AuthListener() {
+
+            public void onAuthError(FirebaseError err) {
+                error("Authentication failed: " + err.getMessage());
+                disable();
             }
-            
-            public void onCancelled(FirebaseError error) {
-                // TODO Auto-generated method stub
+
+            public void onAuthRevoked(FirebaseError err) {
+                error("Authentication revoked: " + err.getMessage());
+                disable();
+            }
+
+            public void onAuthSuccess(Object authData) {
+                FriendCraft.this.authData = authData;
+                
+                // register for bukkit events
+                if (eventListener == null) { eventListener = new EventListener(); }
+                server.getPluginManager().registerEvents(eventListener, FriendCraft.this);
+                
+                log("Version " + getDescription().getVersion() + " enabled");
             }
             
         });
@@ -64,8 +137,30 @@ public class FriendCraft extends JavaPlugin
     public void onDisable()
     {
         // unregister events
-        HandlerList.unregisterAll(this.eventListener);
-        log("Version " + this.getDescription().getVersion() + " disabled");
+        HandlerList.unregisterAll(eventListener);
+        
+        // disconnect from firebase
+        if (authData != null) {
+            log("Disconnecting from Firebase");
+            Firebase rootRef = new Firebase(firebaseRoot);
+            
+            // note: this is currently throwing a ConcurrentModificationException in 1.7.2
+            // i don't know why, but let's just let it do so...
+            rootRef.unauth(new CompletionListener() {
+
+                public void onComplete(FirebaseError err, Firebase ref) {
+                    log("Version " + getDescription().getVersion() + " disabled");
+                }
+                
+            });
+        } else {
+            log("Version " + getDescription().getVersion() + " disabled");
+        }
+    }
+    
+    public void disable()
+    {
+        server.getPluginManager().disablePlugin(this);
     }
 
     public static void log(String msg)
@@ -81,13 +176,5 @@ public class FriendCraft extends JavaPlugin
     public static void error(String msg, Throwable t)
     {
         logger.log(Level.SEVERE, msg, t);
-    }
-
-    public static void debug(String msg)
-    {
-        if (debugMode)
-        {
-            log("[debug] " + msg);
-        }
     }
 }
